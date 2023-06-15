c291d47043af4d06c357aa521bcc6c7cf3429029
ACHRAF <dekham@gmail.com>
2023-06-15 16:15:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts functioin
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like bullding a multilingual puzzle");
+return (0);
+}
